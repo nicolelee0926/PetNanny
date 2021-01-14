7f83cc0952db85bb58c96d5bcef51d1e80bfbb90
nicolelee0926 <nicolelee800926@gmail.com>
2021-01-14 16:49:54 +0800

    modify chat room page textView UI

--- app/src/main/java/com/nicole/petnanny/util/TimeUtil.kt
+++ app/src/main/java/com/nicole/petnanny/util/TimeUtil.kt
@@ -5,7 +5,8 @@ import java.util.*
 
 
 fun Long.toDisplayFormat(): String {
-    val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val formatter = SimpleDateFormat("MM-dd HH:mm")
+//    val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
     formatter.timeZone = TimeZone.getTimeZone("Asia/Taipei")
 //    formatter.timeZone = TimeZone.getTimeZone("GMT :00:00")
     return formatter.format(this)
